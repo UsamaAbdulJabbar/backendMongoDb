6c5b906dd50df2e27b9083633667765ebbbd3b92
Shoaib666 <shoaibejaz117@gmail.com>
2022-08-24 16:22:41 +0500

    work complete

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const userRoute  = require("./routes/userRoute.js");
+const cors = require("cors");
+
+
+mongoose.connect(
+    "mongodb+srv://usamaahmed:usama1527@cluster0.9vjwpbn.mongodb.net/?retryWrites=true&w=majority",
+    (err)=>{
+        if(err){
+            console.log(err);
+        }else{
+            console.log("Mongo Db Connected successfully")
+        }
+    }
+)
+app.use(express.json());
+app.use(cors());
+
+//=====Mongodb Routes==========
+app.use("/user",userRoute);
+
+
+app.listen(5000);
